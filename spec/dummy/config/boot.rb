630395ea3d32d2b929b550e7f5a39d92183a6ecc
fahchen <dev.fah@gmail.com>
2014-04-10 01:16:18 +0800

    Update dummy application to Rails 4.0
    [Issue #6] https://github.com/skinnyworm/wechat-rails/issues/6

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,10 +1,6 @@
-require 'rubygems'
-gemfile = File.expand_path('../../../../Gemfile', __FILE__)
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
 
-if File.exist?(gemfile)
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
-end
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
 $:.unshift File.expand_path('../../../../lib', __FILE__)
