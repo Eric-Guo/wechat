b7991fa5c5182342e982ffe3abeb4f260e1c450c
celery <mingyuan0715@foxmail.com>
2014-06-10 17:48:39 +0800

    require fix

--- lib/wechat-push.rb
+++ lib/wechat-push.rb
@@ -1,5 +1,3 @@
 
-lib = File.expand_path(File.dirname(__FILE__) + '/../bin')
-$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
 
-require 'wechat'
+require_relative '../bin/wechat'
