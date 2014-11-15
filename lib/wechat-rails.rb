ba5d1df9652bdb35a66d2afd46a36beecc44261a
losingle <losingle@gmail.com>
2014-11-15 13:32:59 +0800

    Update wechat-rails.rb

--- lib/wechat-rails.rb
+++ lib/wechat-rails.rb
@@ -31,7 +31,7 @@ module Wechat
   end
 
   def self.api
-    Wechat::Api.new(self.config.appid, self.config.secret, self.config.access_token)
+    @api ||= Wechat::Api.new(self.config.appid, self.config.secret, self.config.access_token)
   end
 end
 
