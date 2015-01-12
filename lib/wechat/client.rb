7ebb0cee0728dc4054036a8e5068037f1bad98fa
Ruoyu Sun <ruoysun@gmail.com>
2015-01-12 18:58:42 +0800

    Update client.rb

--- lib/wechat/client.rb
+++ lib/wechat/client.rb
@@ -35,7 +35,7 @@ module Wechat
         when 0 # for request didn't expect results
           true
 
-        when 42001, 40014 #42001: access_token超时, 40014:不合法的access_token
+        when 42001, 40014, 40001 #42001: access_token超时, 40014:不合法的access_token
           raise AccessTokenExpiredError
           
         else
