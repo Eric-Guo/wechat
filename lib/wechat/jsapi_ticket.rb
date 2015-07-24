265d1b3de76fa3a3a9196eafc2e24f02fb0d717b
feitian124 <feitian124@gmail.com>
2015-07-24 23:18:25 +0800

    signature seems work

--- lib/wechat/jsapi_ticket.rb
+++ lib/wechat/jsapi_ticket.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 module Wechat
   class JsapiTicket
     attr_reader :client, :access_token, :jsapi_ticket_file, :jsapi_ticket_data
@@ -21,6 +23,20 @@ module Wechat
       data = client.get("ticket/getticket", params: { access_token: access_token.token, type: "jsapi"})
       File.open(jsapi_ticket_file, 'w'){|f| f.write(data.to_json)} if valid_ticket(data)
       return @jsapi_ticket_data = data
+    end
+
+    def signature
+      params = {
+        noncestr: 'Wm3WZYTPz0wzccnW',
+        timestamp: 1414587457,
+        jsapi_ticket: 'sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg',
+        url: 'http://mp.weixin.qq.com?params=value'
+      }
+      pairs = params.keys.sort.map do |key|
+        "#{key}=#{params[key]}"
+      end
+      result = Digest::SHA1.hexdigest pairs.join("&")
+      params.merge(signature: result)
     end
 
     private 
