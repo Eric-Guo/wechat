a00116f45f30fd649013b557a30d4a9fd2a61527
Eric Guo <eric.guo@sandisk.com>
2015-08-23 17:42:01 +0800

    Add Rails API support.

--- lib/wechat-rails.rb
+++ lib/wechat-rails.rb
@@ -77,3 +77,30 @@ if defined? ActionController::Base
     end
   end
 end
+
+if defined? ActionController::API
+  class ActionController::API
+    def self.wechat_responder(opts = {})
+      send(:include, Wechat::Responder)
+      if opts.empty?
+        self.corpid = Wechat.config.corpid
+        self.wechat = Wechat.api
+        self.agentid = Wechat.config.agentid
+        self.token = Wechat.config.token
+        self.encrypt_mode = Wechat.config.encrypt_mode
+        self.encoding_aes_key = Wechat.config.encoding_aes_key
+      else
+        self.corpid = opts[:corpid]
+        if corpid.present?
+          self.wechat = Wechat::CorpApi.new(opts[:corpid], opts[:corpsecret], opts[:access_token], opts[:agentid])
+        else
+          self.wechat = Wechat::Api.new(opts[:appid], opts[:secret], opts[:access_token], opts[:jsapi_ticket])
+        end
+        self.agentid = opts[:agentid]
+        self.token = opts[:token]
+        self.encrypt_mode = opts[:encrypt_mode]
+        self.encoding_aes_key = opts[:encoding_aes_key]
+      end
+    end
+  end
+end
