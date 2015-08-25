340f75b89cd3bcf4de79745571d62b7e5763fc4f
Eric Guo <eric.guo@sandisk.com>
2015-08-25 15:24:55 +0800

    Add menu define command for enterprise account.

--- lib/wechat/corp_api.rb
+++ lib/wechat/corp_api.rb
@@ -25,7 +25,15 @@ class Wechat::CorpApi
     get('user/get', params: { userid: userid })
   end
 
-  def menu_create(menu, agentid)
+  def menu
+    get('menu/get', params: { agentid: agentid })
+  end
+
+  def menu_delete
+    get('menu/delete', params: { agentid: agentid })
+  end
+
+  def menu_create(menu)
     # 微信不接受7bit escaped json(eg \uxxxx), 中文必须UTF-8编码, 这可能是个安全漏洞
     post 'menu/create', JSON.generate(menu), { params: { agentid: agentid } }
   end
