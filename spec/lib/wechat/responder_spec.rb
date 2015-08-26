8d3b8166f4ff73903337f402678103e5f4ed76eb
Eric Guo <eric.guo@sandisk.com>
2015-08-26 21:29:12 +0800

    robocop style improve, no logic change

--- spec/lib/wechat/responder_spec.rb
+++ spec/lib/wechat/responder_spec.rb
@@ -5,140 +5,139 @@ class WechatController < ApplicationController
 end
 
 describe WechatController, type: :controller do
-
   def xml_to_hash(response)
-    Hash.from_xml(response.body)["xml"].symbolize_keys
+    Hash.from_xml(response.body)['xml'].symbolize_keys
   end
 
   render_views
 
   let(:signature_params) do
-    timestamp = "1234567"
-    nonce = "nonce"
-    signature = Digest::SHA1.hexdigest([ENV["WECHAT_TOKEN"], timestamp, nonce].sort.join)
-    {timestamp: timestamp, nonce: nonce, signature:signature}
+    timestamp = '1234567'
+    nonce = 'nonce'
+    signature = Digest::SHA1.hexdigest([ENV['WECHAT_TOKEN'], timestamp, nonce].sort.join)
+    { timestamp: timestamp, nonce: nonce, signature: signature }
   end
 
   let(:message_base) do
     {
-      :ToUserName => "toUser",
-      :FromUserName => "fromUser",
-      :CreateTime => "1348831860",
-      :MsgId => "1234567890123456",
+      ToUserName: 'toUser',
+      FromUserName: 'fromUser',
+      CreateTime: '1348831860',
+      MsgId: '1234567890123456'
     }
   end
 
-  let(:text_message){message_base.merge(:MsgType => "text", :Content => "text message")}
+  let(:text_message) { message_base.merge(MsgType: 'text', Content: 'text message') }
 
-  specify "config responder using global config" do
+  specify 'config responder using global config' do
     expect(controller.class.wechat).to eq(Wechat.api)
     expect(controller.class.token).to eq(Wechat.config.token)
   end
 
-  describe "config responder using per controller configuration" do
+  describe 'config responder using per controller configuration' do
     controller do
-      wechat_responder appid: "controller_appid", secret: "controller_secret", token: "controller_token", access_token:"controller_access_token",
-                       agentid: 1, encrypt_mode: false, encoding_aes_key: "encoding_aes_key"
+      wechat_responder appid: 'controller_appid', secret: 'controller_secret', token: 'controller_token',
+                       access_token: 'controller_access_token',
+                       agentid: 1, encrypt_mode: false, encoding_aes_key: 'encoding_aes_key'
     end
-    specify "will set controller wechat api and token" do
+    specify 'will set controller wechat api and token' do
       access_token = controller.class.wechat.access_token
-      expect(access_token.appid).to eq("controller_appid")
-      expect(access_token.secret).to eq("controller_secret")
-      expect(access_token.token_file).to eq("controller_access_token")
-      expect(controller.class.token).to eq("controller_token")
+      expect(access_token.appid).to eq('controller_appid')
+      expect(access_token.secret).to eq('controller_secret')
+      expect(access_token.token_file).to eq('controller_access_token')
+      expect(controller.class.token).to eq('controller_token')
       expect(controller.class.agentid).to eq(1)
       expect(controller.class.encrypt_mode).to eq(false)
-      expect(controller.class.encoding_aes_key).to eq("encoding_aes_key")
+      expect(controller.class.encoding_aes_key).to eq('encoding_aes_key')
     end
   end
 
-  describe "Verify signature" do
-    specify "on show action" do
-      get :show, signature_params.merge(signature:"invalid_signature")
-      expect(response.code).to eq("403")
+  describe 'Verify signature' do
+    specify 'on show action' do
+      get :show, signature_params.merge(signature: 'invalid_signature')
+      expect(response.code).to eq('403')
     end
 
-    specify "on create action" do
-      post :create, signature_params.merge(signature:"invalid_signature")
-      expect(response.code).to eq("403")
+    specify 'on create action' do
+      post :create, signature_params.merge(signature: 'invalid_signature')
+      expect(response.code).to eq('403')
     end
   end
 
   specify "echo 'echostr' param when show" do
-    get :show, signature_params.merge(echostr:"hello")
-    expect(response.body).to eq("hello")
+    get :show, signature_params.merge(echostr: 'hello')
+    expect(response.body).to eq('hello')
   end
 
-  describe "responders" do
-    specify "responders only accept :text, :image, :voice, :video, :location, :link, :event, :fallback message type" do
+  describe 'responders' do
+    specify 'responders only accept :text, :image, :voice, :video, :location, :link, :event, :fallback message type' do
       [:text, :image, :voice, :video, :location, :link, :event, :fallback].each do |message_type|
-        controller.class.on message_type, respond: "response"
+        controller.class.on message_type, respond: 'response'
       end
     end
 
-    specify "will raise error if message type is unkonwn" do
-      expect{controller.class.on :unkonwn, respond: "response"}.to raise_error
+    specify 'will raise error if message type is unkonwn' do
+      expect { controller.class.on :unkonwn, respond: 'response' }.to raise_error
     end
 
-    specify "responder take :with argument only for :text and :event message_type" do
-      expect(controller.class.on :text, with: "command", respond: "response").to eq(with: "command", respond: "response")
-      expect(controller.class.on :event, with: "subscribe").to eq(with: "subscribe")
-      expect{controller.class.on :image, with: "with"}.to raise_error
+    specify 'responder take :with argument only for :text and :event message_type' do
+      expect(controller.class.on :text, with: 'command', respond: 'response').to eq(with: 'command', respond: 'response')
+      expect(controller.class.on :event, with: 'subscribe').to eq(with: 'subscribe')
+      expect { controller.class.on :image, with: 'with' }.to raise_error
     end
   end
 
-  describe "responder_for" do
+  describe 'responder_for' do
     controller do
       wechat_responder
-      on :text,  with: "command", respond: "string matched"
-      on :text,  with: /^cmd:(.*)$/, respond: "regex matched"
-      on :text,  respond: "text content"
-      on :event, with: "subscribe", respond: "subscribe event"
-      on :image, respond: "image content"
+      on :text,  with: 'command', respond: 'string matched'
+      on :text,  with: /^cmd:(.*)$/, respond: 'regex matched'
+      on :text,  respond: 'text content'
+      on :event, with: 'subscribe', respond: 'subscribe event'
+      on :image, respond: 'image content'
     end
 
-    specify "find first responder for matched type" do
-      expect{|b|
-        controller.class.responder_for(:MsgType=> "image", &b)
-      }.to yield_with_args({:respond => "image content"})
+    specify 'find first responder for matched type' do
+      expect do |b|
+        controller.class.responder_for(MsgType: 'image', &b)
+      end.to yield_with_args(respond: 'image content')
     end
 
     specify "find 'general text' responder if none of the text responders matches request content" do
-      expect{|b|
-        controller.class.responder_for(:MsgType=> "text", :Content=> "some text", &b)
-      }.to yield_with_args({:respond => "text content"}, "some text")
+      expect do |b|
+        controller.class.responder_for(MsgType: 'text', Content: 'some text', &b)
+      end.to yield_with_args({ respond: 'text content' }, 'some text')
     end
 
     specify "find 'string matched' responder if request content matches string" do
-      expect{|b|
-        controller.class.responder_for(:MsgType=> "text", :Content=> "command", &b)
-      }.to yield_with_args({:respond => "string matched", with: "command"}, "command")
+      expect do |b|
+        controller.class.responder_for(MsgType: 'text', Content: 'command', &b)
+      end.to yield_with_args({ respond: 'string matched', with: 'command' }, 'command')
     end
 
     specify "find 'regex mached' responder if request content matches regex" do
-      expect{|b|
-        controller.class.responder_for(:MsgType=> "text", :Content=> "cmd:my_command", &b)
-      }.to yield_with_args({:respond => "regex matched", with: /^cmd:(.*)$/}, "my_command")
+      expect do |b|
+        controller.class.responder_for(MsgType: 'text', Content: 'cmd:my_command', &b)
+      end.to yield_with_args({ respond: 'regex matched', with: /^cmd:(.*)$/ }, 'my_command')
     end
 
     specify "find 'subscribe event' responder if event request matches event" do
-      expect{|b|
-        controller.class.responder_for(:MsgType=> "event", :Event=> "subscribe", &b)
-      }.to yield_with_args({:respond => "subscribe event", with: "subscribe"}, "subscribe")
+      expect do |b|
+        controller.class.responder_for(MsgType: 'event', Event: 'subscribe', &b)
+      end.to yield_with_args({ respond: 'subscribe event', with: 'subscribe' }, 'subscribe')
     end
-
   end
 
-  specify "will respond empty if no responder for the message type" do
+  specify 'will respond empty if no responder for the message type' do
     post :create, signature_params.merge(xml: text_message)
-    expect(response.code).to eq("200")
+    expect(response.code).to eq('200')
     expect(response.body.strip).to be_empty
   end
 
   describe 'respond_to wechat_url helper' do
     controller do
       wechat_responder
-      on :text do |message, content|
+      on :text do |_message, _content|
         wechat_url
       end
     end
@@ -150,36 +149,34 @@ describe WechatController, type: :controller do
     end
   end
 
-  describe "fallback responder" do
+  describe 'fallback responder' do
     controller do
       wechat_responder
-      on :fallback, respond: "fallback responder"
+      on :fallback, respond: 'fallback responder'
     end
 
-    specify "will respond to any message" do
+    specify 'will respond to any message' do
       post :create, signature_params.merge(xml: text_message)
-      expect(xml_to_hash(response)[:Content]).to eq("fallback responder")
+      expect(xml_to_hash(response)[:Content]).to eq('fallback responder')
     end
   end
 
-
-  describe "#create use cases" do
-
+  describe '#create use cases' do
     controller do
       wechat_responder
-      on :text, respond: "text message" do |message, content|
-        message.replay.text("should not be here")
+      on :text, respond: 'text message' do |message, _content|
+        message.replay.text('should not be here')
       end
 
-      on :text,  with: "command" do |message, content|
+      on :text, with: 'command' do |message, content|
         message.reply.text("text: #{content}")
       end
 
-      on :text,  with: /^cmd:(.*)$/ do |message, cmd|
+      on :text, with: /^cmd:(.*)$/ do |message, cmd|
         message.reply.text("text: #{cmd}")
       end
 
-      on :event, with: "subscribe" do |message, event|
+      on :event, with: 'subscribe' do |message, event|
         message.reply.text("event: #{event}")
       end
 
@@ -202,63 +199,60 @@ describe WechatController, type: :controller do
       on :link do |message|
         message.reply.text("link: #{message[:Url]}")
       end
-
     end
 
-    specify "response with respond field" do
-      post :create, signature_params.merge(xml: text_message.merge(:Content=>"message"))
+    specify 'response with respond field' do
+      post :create, signature_params.merge(xml: text_message.merge(Content: 'message'))
       result = xml_to_hash(response)
-      expect(result[:ToUserName]).to eq("fromUser")
-      expect(result[:FromUserName]).to eq("toUser")
-      expect(result[:Content]).to eq("text message")
+      expect(result[:ToUserName]).to eq('fromUser')
+      expect(result[:FromUserName]).to eq('toUser')
+      expect(result[:Content]).to eq('text message')
     end
 
-    specify "response text with text match" do
-      post :create, signature_params.merge(xml: text_message.merge(:Content=>"command"))
-      expect(xml_to_hash(response)[:Content]).to eq("text: command")
+    specify 'response text with text match' do
+      post :create, signature_params.merge(xml: text_message.merge(Content: 'command'))
+      expect(xml_to_hash(response)[:Content]).to eq('text: command')
     end
 
-    specify "response text with regex matched" do
-      post :create, signature_params.merge(xml: text_message.merge(:Content=>"command"))
-      expect(xml_to_hash(response)[:Content]).to eq("text: command")
+    specify 'response text with regex matched' do
+      post :create, signature_params.merge(xml: text_message.merge(Content: 'command'))
+      expect(xml_to_hash(response)[:Content]).to eq('text: command')
     end
 
-    specify "response event with matched event" do
-      event_message = message_base.merge(:MsgType => "event", :Event=>"VIEW")
-      post :create, signature_params.merge(xml: event_message.merge(:Event=>"subscribe"))
-      expect(xml_to_hash(response)[:Content]).to eq("event: subscribe")
+    specify 'response event with matched event' do
+      event_message = message_base.merge(MsgType: 'event', Event: 'VIEW')
+      post :create, signature_params.merge(xml: event_message.merge(Event: 'subscribe'))
+      expect(xml_to_hash(response)[:Content]).to eq('event: subscribe')
     end
 
-    specify "response image" do
-      image_message = message_base.merge(:MsgType => "image", :MediaId=>"image_media_id", :PicUrl => "pic_url")
+    specify 'response image' do
+      image_message = message_base.merge(MsgType: 'image', MediaId: 'image_media_id', PicUrl: 'pic_url')
       post :create, signature_params.merge(xml: image_message)
-      expect(xml_to_hash(response)[:Content]).to eq("image: pic_url")
+      expect(xml_to_hash(response)[:Content]).to eq('image: pic_url')
     end
 
-    specify "response voice" do
-      message = message_base.merge(:MsgType => "voice", :MediaId=>"voice_media_id", :Format=>"format")
+    specify 'response voice' do
+      message = message_base.merge(MsgType: 'voice', MediaId: 'voice_media_id', Format: 'format')
       post :create, signature_params.merge(xml: message)
-      expect(xml_to_hash(response)[:Content]).to eq("voice: voice_media_id")
+      expect(xml_to_hash(response)[:Content]).to eq('voice: voice_media_id')
     end
 
-    specify "response video" do
-      message = message_base.merge(:MsgType => "video", :MediaId=>"video_media_id", :ThumbMediaId=>"thumb_media_id")
+    specify 'response video' do
+      message = message_base.merge(MsgType: 'video', MediaId: 'video_media_id', ThumbMediaId: 'thumb_media_id')
       post :create, signature_params.merge(xml: message)
-      expect(xml_to_hash(response)[:Content]).to eq("video: video_media_id")
+      expect(xml_to_hash(response)[:Content]).to eq('video: video_media_id')
     end
 
-    specify "response location" do
-      message = message_base.merge(:MsgType => "location", :Location_X=>"location_x", :Location_Y=>"location_y", :Scale=>"scale", :Label=>"label")
+    specify 'response location' do
+      message = message_base.merge(MsgType: 'location', Location_X: 'location_x', Location_Y: 'location_y', Scale: 'scale', Label: 'label')
       post :create, signature_params.merge(xml: message)
-      expect(xml_to_hash(response)[:Content]).to eq("location: label")
+      expect(xml_to_hash(response)[:Content]).to eq('location: label')
     end
 
-    specify "response link" do
-      message = message_base.merge(:MsgType => "link", :Url=>"link_url", :Title=>"title", :Description=>"description")
+    specify 'response link' do
+      message = message_base.merge(MsgType: 'link', Url: 'link_url', Title: 'title', Description: 'description')
       post :create, signature_params.merge(xml: message)
-      expect(xml_to_hash(response)[:Content]).to eq("link: link_url")
+      expect(xml_to_hash(response)[:Content]).to eq('link: link_url')
     end
-
   end
-
 end
