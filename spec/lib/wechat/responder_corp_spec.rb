81b78316d2cdf3bbccebdf3e0e8b7ddd3fd9f05e
Eric Guo <eric.guo@sandisk.com>
2015-08-26 21:41:34 +0800

    robocop style improve, no logic change

--- spec/lib/wechat/responder_corp_spec.rb
+++ spec/lib/wechat/responder_corp_spec.rb
@@ -5,20 +5,19 @@ include Wechat::Cipher
 ENCODING_AES_KEY = Base64.encode64 SecureRandom.hex(16)
 
 class WechatCorpController < ApplicationController
-  wechat_responder appid: "appid", secret: "secret", token: "token", access_token: "controller_access_token",
+  wechat_responder appid: 'appid', secret: 'secret', token: 'token', access_token: 'controller_access_token',
                    agentid: 1, encrypt_mode: true, encoding_aes_key: ENCODING_AES_KEY
 end
 
 describe WechatCorpController, type: :controller do
-
   render_views
 
   let(:message_base) do
     {
-        :ToUserName => "toUser",
-        :FromUserName => "fromUser",
-        :CreateTime => "1348831860",
-        :MsgId => "1234567890123456",
+      ToUserName: 'toUser',
+      FromUserName: 'fromUser',
+      CreateTime: '1348831860',
+      MsgId: '1234567890123456'
     }
   end
 
@@ -26,41 +25,39 @@ describe WechatCorpController, type: :controller do
     xml = message_base.merge(msg).to_xml(root: :xml, skip_instruct: true)
 
     encrypt = Base64.strict_encode64 encrypt(pack(xml, 'appid'), ENCODING_AES_KEY)
-    xml = {Encrypt: encrypt}
-    timestamp = "1234567"
-    nonce = "nonce"
+    xml = { Encrypt: encrypt }
+    timestamp = '1234567'
+    nonce = 'nonce'
     msg_signature = Digest::SHA1.hexdigest(['token', timestamp, nonce, xml[:Encrypt]].sort.join)
-    {timestamp: timestamp, nonce: nonce, xml: xml, msg_signature: msg_signature}
+    { timestamp: timestamp, nonce: nonce, xml: xml, msg_signature: msg_signature }
   end
 
   def xml_to_hash(response)
-    Hash.from_xml(response.body)["xml"].symbolize_keys
+    Hash.from_xml(response.body)['xml'].symbolize_keys
   end
 
-
   describe 'corp' do
-
     controller do
-      wechat_responder corpid: "corpid", corpsecret: "corpsecret", token: "token", access_token:"controller_access_token",
+      wechat_responder corpid: 'corpid', corpsecret: 'corpsecret', token: 'token', access_token: 'controller_access_token',
                        agentid: 1, encrypt_mode: false, encoding_aes_key: ENCODING_AES_KEY
 
       on :text do |request, content|
         request.reply.text "echo: #{content}"
       end
 
-      on :event, with: 'my_event' do |request, key|
-        request.reply.text "echo: my_event"
+      on :event, with: 'my_event' do |request, _key|
+        request.reply.text 'echo: my_event'
       end
     end
 
     describe 'Verify signature' do
-      it "on create action faild" do
+      it 'on create action faild' do
         post :create, signature_params.merge(msg_signature: 'invalid')
         expect(response.code).to eq '403'
       end
 
-      it "on create action success" do
-        post :create, signature_params({MsgType: 'voice', Voice: {MediaId: 'mediaID'}})
+      it 'on create action success' do
+        post :create, signature_params(MsgType: 'voice', Voice: { MediaId: 'mediaID' })
         expect(response.code).to eq '200'
         expect(response.body.length).to eq 0
       end
@@ -68,7 +65,7 @@ describe WechatCorpController, type: :controller do
 
     describe 'response' do
       it 'Verify response signature' do
-        post :create, signature_params({MsgType: 'text', Content: 'hello'})
+        post :create, signature_params(MsgType: 'text', Content: 'hello')
         expect(response.code).to eq '200'
         expect(response.body.empty?).to eq false
 
@@ -79,7 +76,7 @@ describe WechatCorpController, type: :controller do
       end
 
       it 'on text' do
-        post :create, signature_params({MsgType: 'text', Content: 'hello'})
+        post :create, signature_params(MsgType: 'text', Content: 'hello')
         expect(response.code).to eq '200'
         expect(response.body.empty?).to eq false
 
@@ -95,7 +92,7 @@ describe WechatCorpController, type: :controller do
       end
 
       it 'on event' do
-        post :create, signature_params({MsgType: 'event', Event: 'click', EventKey: 'my_event'})
+        post :create, signature_params(MsgType: 'event', Event: 'click', EventKey: 'my_event')
         expect(response.code).to eq '200'
 
         data = Hash.from_xml(response.body)['xml']
