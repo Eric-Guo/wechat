4b14057cf20f3328a2cfe1ba17a4a906e82440d0
Eric Guo <eric.guo@sandisk.com>
2015-09-01 14:59:44 +0800

    Refresh dummy rails app from Rails-API

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,22 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
 require "rails"
-
-%w(
-  action_controller
-  action_mailer
-  sprockets
-).each do |framework|
-  begin
-    require "#{framework}/railtie"
-  rescue LoadError
-  end
-end
+# Pick the frameworks you want:
+#require "active_model/railtie"
+#require "active_job/railtie"
+#require "active_record/railtie"
+require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "action_view/railtie"
+# require "sprockets/railtie"
+#require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "wechat"
 
 module Dummy
   class Application < Rails::Application
@@ -31,5 +28,8 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -2,6 +2,18 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '3edd61657bac2d472f9e19d38ee3ff5d4fd8636fe6e9352e516cc11bb1f6477a7024fbfc5087665202fc5efbc5eed79419c2cd892dfcf82f619f9e037d92ea5a'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Dummy::Application.config.secret_token = '095a4e2b350f8c091daa820a82aad8f732c7444b4820d2dfdcdf35f76c88f905145702a15258799aa39d0a48a4bda38df6da3f51b40ec0733c72a8269934cdb7'
