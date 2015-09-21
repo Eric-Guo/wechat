5adbd122910b92a9f67c09aed538ac267a4a5c8d
Eric Guo <eric.guo@sandisk.com>
2015-09-21 12:16:27 +0800

    Will add route and copy wechat controller when install also.

--- lib/generators/wechat/install_generator.rb
+++ lib/generators/wechat/install_generator.rb
@@ -1,11 +1,19 @@
 module Wechat
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      desc "Copy Wechat default files"
+      desc 'Install Wechat support files'
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_config
-        template "config/wechat.yml"
+        template 'config/wechat.yml'
+      end
+
+      def add_wechat_route
+        route 'resource :wechat, only: [:show, :create]'
+      end
+
+      def copy_wechat_controller
+        template 'app/controllers/wechats_controller.rb'
       end
     end
   end
---
+++ lib/generators/wechat/templates/app/controllers/wechats_controller.rb
@@ -0,0 +1,91 @@
+class WechatsController < ApplicationController
+  wechat_responder
+
+  # default text responder when no other match
+  on :text do |request, content|
+    request.reply.text "echo: #{content}" # Just echo
+  end
+
+  # When receive 'help', will trigger this responder
+  on :text, with: 'help' do |request|
+    request.reply.text 'help content'
+  end
+
+  # When receive '<n>news', will match and will got count as <n> as parameter
+  on :text, with: /^(\d+) news$/ do |request, count|
+    # Wechat article can only contain max 10 items, large than 10 will dropped.
+    news = (1..count.to_i).each_with_object([]) { |n, memo| memo << { title: 'News title', content: "No. #{n} news content" } }
+    request.reply.news(news) do |article, n, index| # article is return object
+      article.item title: "#{index} #{n[:title]}", description: n[:content], pic_url: 'http://www.baidu.com/img/bdlogo.gif', url: 'http://www.baidu.com/'
+    end
+  end
+
+  # When user press menu BINDING_QR_CODE and success to scan bar code
+  on :event, with: 'BINDING_QR_CODE' do |request, scan_result, scan_type|
+    request.reply.text "User #{request[:FromUserName]} ScanResult #{scan_result} ScanType #{scan_type}"
+  end
+
+  # Except QR code, wechat can also scan CODE_39 bar code
+  on :event, with: 'BINDING_BARCODE' do |message, scan_result|
+    if scan_result.start_with? 'CODE_39,'
+      message.reply.text "User: #{message[:FromUserName]} scan barcode, result is #{scan_result.split(',')[1]}"
+    end
+  end
+
+  # When user sent the imsage
+  on :image do |request|
+    request.reply.image(request[:MediaId]) # Echo the sent image to user
+  end
+
+  # When user sent the voice
+  on :voice do |request|
+    request.reply.voice(request[:MediaId]) # Echo the sent voice to user
+  end
+
+  # When user sent the video
+  on :video do |request|
+    nickname = wechat.user(request[:FromUserName])['nickname'] # Call wechat api to get sender nickname
+    request.reply.video(request[:MediaId], title: 'Echo', description: "Got #{nickname} sent video") # Echo the sent video to user
+  end
+
+  # When user sent location
+  on :location do |request|
+    request.reply.text("#{request[:Location_X]}, #{request[:Location_Y]}") # replay the GPS location
+  end
+
+  on :event, with: 'subscribe' do |request|
+    request.reply.text "#{request[:FromUserName]} subscribe now"
+  end
+
+  on :event, with: 'unsubscribe' do |request|
+    request.reply.text "#{request[:FromUserName]} can not receive this message"
+  end
+
+  # When user enter the app / agent app
+  on :event, with: 'enter_agent' do |request|
+    request.reply.text "#{request[:FromUserName]} enter agent app now"
+  end
+
+  # When batch job create/update user (incremental) finished.
+  on :event, with: 'sync_user' do |request, batch_job|
+    request.reply.text "job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # When batch job replace user (full sync) finished.
+  on :event, with: 'replace_user' do |request, batch_job|
+    request.reply.text "job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # When batch job invent user finished.
+  on :event, with: 'invite_user' do |request, batch_job|
+    request.reply.text "job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # When batch job replace department (full sync) finished.
+  on :event, with: 'replace_party' do |request, batch_job|
+    request.reply.text "job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # Any not match above will fail to below
+  on :fallback, respond: 'fallback message'
+end
