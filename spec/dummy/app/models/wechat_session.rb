22e6e44f0e00e3273ec2641f9c9eef36eefcce92
Eric Guo <eric.guo@sandisk.com>
2016-01-18 00:56:22 +0800

    Add two comments in user model wechat_session file.

--- spec/dummy/app/models/wechat_session.rb
+++ spec/dummy/app/models/wechat_session.rb
@@ -3,10 +3,12 @@ class WechatSession < ActiveRecord::Base
   validates :openid, presence: true, uniqueness: true
   serialize :hash_store, Hash
 
+  # called by wechat gems when user request session
   def self.find_or_initialize_session(request_message)
     find_or_initialize_by(openid: request_message[:from_user_name])
   end
 
+  # called by wechat gems after response Techent server at controller#create
   def save_session(response_message)
     save!
   end
