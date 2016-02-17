813740aacadc93f40b08c13f4821506276fcbc8d
Eric Guo <eric.guo@sandisk.com>
2016-02-17 15:17:52 +0800

    To make rubocop happy.

--- lib/action_controller/wechat_responder.rb
+++ lib/action_controller/wechat_responder.rb
@@ -11,15 +11,11 @@ module ActionController
       self.token = opts[:token] || Wechat.config.token
       self.encoding_aes_key = opts[:encoding_aes_key] || Wechat.config.encoding_aes_key
 
-      if opts.empty?
-        self.wechat = Wechat.api
-      else
-        if corpid.present?
-          self.wechat = Wechat::CorpApi.new(corpid, opts[:corpsecret], opts[:access_token], agentid, timeout, skip_verify_ssl, opts[:jsapi_ticket])
-        else
-          self.wechat = Wechat::Api.new(opts[:appid], opts[:secret], opts[:access_token], timeout, skip_verify_ssl, opts[:jsapi_ticket])
-        end
-      end
+      return self.wechat = Wechat.api if opts.empty?
+      return self.wechat = Wechat::CorpApi.new(corpid, opts[:corpsecret], opts[:access_token], \
+                                               agentid, timeout, skip_verify_ssl, opts[:jsapi_ticket]) if corpid.present?
+      self.wechat = Wechat::Api.new(opts[:appid], opts[:secret], opts[:access_token], \
+                                    timeout, skip_verify_ssl, opts[:jsapi_ticket])
     end
   end
 
