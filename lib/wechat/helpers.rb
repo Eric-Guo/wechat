7431b70163a1b62e968558100dfe235b8d2b5258
Eric Guo <eric.guo@sandisk.com>
2016-03-28 20:54:17 +0800

    Using controller.wechat to access jsapi_ticket to support mutl-account.

--- lib/wechat/helpers.rb
+++ lib/wechat/helpers.rb
@@ -1,12 +1,12 @@
 module Wechat
   module Helpers
     def wechat_config_js(config_options = {})
-      page_url = if Wechat.config.trusted_domain_fullname
-                   "#{Wechat.config.trusted_domain_fullname}#{controller.request.original_fullpath}"
+      page_url = if controller.class.trusted_domain_fullname
+                   "#{controller.class.trusted_domain_fullname}#{controller.request.original_fullpath}"
                  else
                    controller.request.original_url
                  end
-      js_hash = Wechat.api.jsapi_ticket.signature(page_url)
+      js_hash = controller.wechat.jsapi_ticket.signature(page_url)
       config_js = <<-WECHAT_CONFIG_JS
 wx.config({
   debug: #{config_options[:debug]},
--- spec/lib/wechat/helpers_spec.rb
+++ spec/lib/wechat/helpers_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
+class WechatApiController < ActionController::Base
+  wechat_api
+end
+
 RSpec.describe Wechat::Helpers do
   include Wechat::Helpers
   include ActionView::Helpers::JavaScriptHelper
   include ActionView::Helpers::TagHelper
-  let(:controller) { ActionController::Base.new }
+  let(:controller) { WechatApiController.new }
 
   it '#wechat_config_js' do
     js_hash_result = { timestamp: 'timestamp', noncestr: 'noncestr', signature: 'signature' }
     controller.request = ActionController::TestRequest.new(host: 'test.host')
-    expect(Wechat.api.jsapi_ticket).to receive(:signature)
+    expect(controller.wechat.jsapi_ticket).to receive(:signature)
       .with('http://test.host').and_return(js_hash_result)
     expect(wechat_config_js(debug: false, api: %w(hideMenuItems openEnterpriseChat))).to end_with '</script>'
   end
