9c3989abea5a29f2d1a4bf6208d3c8db2ee42d4d
Eric Guo <eric.guo@sandisk.com>
2016-04-04 15:03:05 +0800

    Testing wechat_config_js with trusted_domain_fullname

--- spec/lib/wechat/helpers_spec.rb
+++ spec/lib/wechat/helpers_spec.rb
@@ -1,20 +1,37 @@
 require 'spec_helper'
 
 class WechatApiController < ActionController::Base
-  wechat_api
 end
 
-RSpec.describe Wechat::Helpers do
+RSpec.describe WechatApiController, type: :controller do
   include Wechat::Helpers
   include ActionView::Helpers::JavaScriptHelper
   include ActionView::Helpers::TagHelper
-  let(:controller) { WechatApiController.new }
+  let(:js_hash_result) { { timestamp: 'timestamp', noncestr: 'noncestr', signature: 'signature' } }
 
-  it '#wechat_config_js' do
-    js_hash_result = { timestamp: 'timestamp', noncestr: 'noncestr', signature: 'signature' }
-    controller.request = ActionController::TestRequest.new(host: 'test.host')
-    expect(controller.wechat.jsapi_ticket).to receive(:signature)
-      .with('http://test.host').and_return(js_hash_result)
-    expect(wechat_config_js(debug: false, api: %w(hideMenuItems openEnterpriseChat))).to end_with '</script>'
+  describe '#wechat_config_js with original_url' do
+    controller do
+      wechat_api
+    end
+
+    it '#wechat_config_js' do
+      controller.request = ActionController::TestRequest.new(host: 'test.host')
+      expect(controller.wechat.jsapi_ticket).to receive(:signature)
+        .with('http://test.host').and_return(js_hash_result)
+      expect(wechat_config_js(debug: false, api: %w(hideMenuItems openEnterpriseChat))).to end_with '</script>'
+    end
+  end
+
+  describe '#wechat_config_js with trusted_domain_fullname' do
+    controller do
+      wechat_api trusted_domain_fullname: 'http://trusted.host'
+    end
+
+    it 'called with trusted_domain' do
+      controller.request = ActionController::TestRequest.new(host: 'trusted.host')
+      expect(controller.wechat.jsapi_ticket).to receive(:signature)
+        .with('http://trusted.host').and_return(js_hash_result)
+      expect(wechat_config_js(debug: false, api: %w(hideMenuItems))).to end_with '</script>'
+    end
   end
 end
