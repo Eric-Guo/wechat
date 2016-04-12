b6cab11b4d78259e22d79b513140cf268d528f55
Eric Guo <eric.guo@sandisk.com>
2016-04-12 20:24:41 +0800

    Also need record the @oauth2_state at same time, to fix #103.

--- lib/wechat/ticket/corp_jsapi_ticket.rb
+++ lib/wechat/ticket/corp_jsapi_ticket.rb
@@ -5,7 +5,7 @@ module Wechat
     class CorpJsapiTicket < JsapiBase
       def refresh
         data = client.get('get_jsapi_ticket', params: { access_token: access_token.token })
-        data['oauth2_state'] = SecureRandom.hex(16)
+        @oauth2_state = data['oauth2_state'] = SecureRandom.hex(16)
         write_ticket_to_store(data)
         read_ticket_from_store
       end
--- lib/wechat/ticket/public_jsapi_ticket.rb
+++ lib/wechat/ticket/public_jsapi_ticket.rb
@@ -5,7 +5,7 @@ module Wechat
     class PublicJsapiTicket < JsapiBase
       def refresh
         data = client.get('ticket/getticket', params: { access_token: access_token.token, type: 'jsapi' })
-        data['oauth2_state'] = SecureRandom.hex(16)
+        @oauth2_state = data['oauth2_state'] = SecureRandom.hex(16)
         write_ticket_to_store(data)
         read_ticket_from_store
       end
