9edac30810daa146f96608d06b589b02cbce4897
Snow Helsing <snow.helsing@gmail.com>
2016-07-27 10:38:55 +0800

    FIX: consider '' in params as equal with nil, in ControllerApi#wechat_public_oauth2

--- lib/wechat/controller_api.rb
+++ lib/wechat/controller_api.rb
@@ -34,7 +34,9 @@ module Wechat
     def wechat_public_oauth2(oauth2_url)
       if cookies.signed_or_encrypted[:we_openid].blank? && params[:code].blank?
         redirect_to oauth2_url
-      elsif cookies.signed_or_encrypted[:we_openid].blank? && params[:code].present? && params[:state] == wechat.jsapi_ticket.oauth2_state
+      elsif cookies.signed_or_encrypted[:we_openid].blank? &&
+            params[:code].present? &&
+            params[:state].to_s == wechat.jsapi_ticket.oauth2_state.to_s # params[:state] maybe nil and wechat.jsapi_ticket.oauth2_state may be nil
         access_info = wechat.web_access_token(params[:code])
         cookies.signed_or_encrypted[:we_openid] = { value: access_info['openid'], expires: self.class.oauth2_cookie_duration.from_now }
         yield access_info['openid'], access_info
