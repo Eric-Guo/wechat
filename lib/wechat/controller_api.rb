0fc51a5eebbaca09e4bb41802aaf84588d41552e
IChou <me@ichou.cn>
2016-11-19 18:19:42 +0800

    fix oauth2_state bug in wechat_oauth2 optimize controller_api

--- lib/wechat/controller_api.rb
+++ lib/wechat/controller_api.rb
@@ -12,62 +12,68 @@ module Wechat
     end
 
     def wechat_oauth2(scope = 'snsapi_base', page_url = nil, &block)
-      appid = self.class.corpid || self.class.appid
-      if appid.blank?
+      appid = self.class.corpid || self.class.appid || -> {
         self.class.wechat # to initialize wechat_api_client at first time call wechat_oauth2
-        appid = self.class.corpid || self.class.appid
-        raise 'Can not get corpid or appid, so please configure it first to using wechat_oauth2' if appid.blank?
-      end
-      page_url ||= if self.class.trusted_domain_fullname
-                     "#{self.class.trusted_domain_fullname}#{request.original_fullpath}"
-                   else
-                     request.original_url
-                   end
-      redirect_uri = CGI.escape(page_url)
-      if scope == 'snsapi_login'
-        oauth2_url = "https://open.weixin.qq.com/connect/qrconnect?appid=#{appid}&redirect_uri=#{redirect_uri}&response_type=code&scope=#{scope}&state=#{wechat.jsapi_ticket.oauth2_state}#wechat_redirect"
-      else
-        oauth2_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{appid}&redirect_uri=#{redirect_uri}&response_type=code&scope=#{scope}&state=#{wechat.jsapi_ticket.oauth2_state}#wechat_redirect"
-      end
+        self.class.corpid || self.class.appid
+      }.call
+      raise 'Can not get corpid or appid, so please configure it first to using wechat_oauth2' if appid.blank?
 
-      return oauth2_url unless block_given?
-      if self.class.corpid
-        wechat_corp_oauth2(oauth2_url, &block)
-      else
-        wechat_public_oauth2(oauth2_url, &block)
-      end
+      wechat.jsapi_ticket.ticket if wechat.jsapi_ticket.oauth2_state.nil?
+      oauth2_params = {
+          appid: appid,
+          redirect_uri: page_url,
+          scope: scope,
+          response_type: 'code',
+          state: wechat.jsapi_ticket.oauth2_state
+      }
+
+      return generate_oauth2_url(oauth2_params) unless block_given?
+      self.class.corpid ? wechat_corp_oauth2(oauth2_params, &block) : wechat_public_oauth2(oauth2_params, &block)
     end
 
     private
 
-    def wechat_public_oauth2(oauth2_url)
-      if cookies.signed_or_encrypted[:we_openid].blank? && params[:code].blank?
-        redirect_to oauth2_url
-      elsif cookies.signed_or_encrypted[:we_openid].blank? &&
-            params[:code].present? &&
-            params[:state].to_s == wechat.jsapi_ticket.oauth2_state.to_s # params[:state] maybe '' and wechat.jsapi_ticket.oauth2_state may be nil
+    def wechat_public_oauth2(oauth2_params)
+      openid, unionid = cookies.signed_or_encrypted[:we_openid], cookies.signed_or_encrypted[:we_unionid]
+      if openid.present?
+        yield openid, { 'openid' => openid, 'unionid' => unionid }
+      elsif params[:code].present? && params[:state] == oauth2_params[:state]
         access_info = wechat.web_access_token(params[:code])
         cookies.signed_or_encrypted[:we_openid] = { value: access_info['openid'], expires: self.class.oauth2_cookie_duration.from_now }
         cookies.signed_or_encrypted[:we_unionid] = { value: access_info['unionid'], expires: self.class.oauth2_cookie_duration.from_now }
         yield access_info['openid'], access_info
       else
-        yield cookies.signed_or_encrypted[:we_openid], { 'openid' => cookies.signed_or_encrypted[:we_openid],
-                                                         'unionid' => cookies.signed_or_encrypted[:we_unionid] }
+        redirect_to generate_oauth2_url(oauth2_params)
       end
     end
 
-    def wechat_corp_oauth2(oauth2_url)
-      if cookies.signed_or_encrypted[:we_deviceid].blank? && params[:code].blank?
-        redirect_to oauth2_url
-      elsif cookies.signed_or_encrypted[:we_deviceid].blank? && params[:code].present? && params[:state] == wechat.jsapi_ticket.oauth2_state
+    def wechat_corp_oauth2(oauth2_params)
+      userid, deviceid = cookies.signed_or_encrypted[:we_userid], cookies.signed_or_encrypted[:we_deviceid]
+      if userid.present? && deviceid.present?
+        yield userid, { 'UserId' => userid, 'DeviceId' => deviceid }
+      elsif params[:code].present? && params[:state] == oauth2_params[:state]
         userinfo = wechat.getuserinfo(params[:code])
         cookies.signed_or_encrypted[:we_userid] = { value: userinfo['UserId'], expires: self.class.oauth2_cookie_duration.from_now }
         cookies.signed_or_encrypted[:we_deviceid] = { value: userinfo['DeviceId'], expires: self.class.oauth2_cookie_duration.from_now }
         yield userinfo['UserId'], userinfo
       else
-        yield cookies.signed_or_encrypted[:we_userid], { 'UserId' => cookies.signed_or_encrypted[:we_userid],
-                                                         'DeviceId' => cookies.signed_or_encrypted[:we_deviceid] }
+        redirect_to generate_oauth2_url(oauth2_params)
       end
     end
+
+    def generate_oauth2_url(oauth2_params)
+      if oauth2_params[:redirect_uri].blank?
+        page_url = (td = self.class.trusted_domain_fullname) ? "#{td}#{request.original_fullpath}" : request.original_url
+        safe_query = request.query_parameters.reject { |k, _| %w(code state access_token).include? k }.to_query
+        oauth2_params[:redirect_uri] = page_url.sub(request.query_string, safe_query)
+      end
+
+      if oauth2_params[:scope] == 'snsapi_login'
+        "https://open.weixin.qq.com/connect/qrconnect?#{oauth2_params.to_query}#wechat_redirect"
+      else
+        "https://open.weixin.qq.com/connect/oauth2/authorize?#{oauth2_params.to_query}#wechat_redirect"
+      end
+    end
+
   end
 end
