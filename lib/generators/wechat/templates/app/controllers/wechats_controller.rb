7f166b5fcf69640ba9274c0deb27c8c4e76bd369
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-29 23:21:31 +0800

    Review links

--- lib/generators/wechat/templates/app/controllers/wechats_controller.rb
+++ lib/generators/wechat/templates/app/controllers/wechats_controller.rb
@@ -3,7 +3,7 @@ class WechatsController < ApplicationController
 <% else -%>
 class WechatsController < ActionController::Base
 <% end -%>
-  # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-controller-dsl
+  # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#wechat_responder---rails-responder-controller-dsl
   wechat_responder
 
   on :text do |request, content|
