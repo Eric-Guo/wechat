0ae9c47a6faf6c15bd1879f60a58cfd6fc5c6063
Liren Tu <ltu@liveramp.com>
2018-04-08 20:36:24 -0700

    Fix migration helper

--- lib/generators/wechat/config_generator.rb
+++ lib/generators/wechat/config_generator.rb
@@ -1,16 +1,17 @@
 require 'rails/generators/active_record'
+require File.expand_path('../migration_helper', __FILE__)
 
 module Wechat
   module Generators
     class ConfigGenerator < Rails::Generators::Base
       include ::Rails::Generators::Migration
-      include MigrationHelper
+      include Wechat::Generators::MigrationHelper
 
       desc 'Generate wechat configs in database'
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_wechat_config_migration
-        version = ActiveRecord::Migration.current_version
+        version = ActiveRecord::VERSION::STRING.to_f
         source = version >= 5 ? create_migration_with_version(version, 'config_migration') : 'db/config_migration.rb'
         migration_template source, 'db/migrate/create_wechat_configs.rb'
       end
--- lib/generators/wechat/session_generator.rb
+++ lib/generators/wechat/session_generator.rb
@@ -1,16 +1,17 @@
 require 'rails/generators/active_record'
+require File.expand_path('../migration_helper', __FILE__)
 
 module Wechat
   module Generators
     class SessionGenerator < Rails::Generators::Base
       include ::Rails::Generators::Migration
-      include MigrationHelper
+      include Wechat::Generators::MigrationHelper
 
       desc 'Enable wechat session support'
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_wechat_sessions_migration
-        version = ActiveRecord::Migration.current_version
+        version = ActiveRecord::VERSION::STRING.to_f
         source = version >= 5 ? create_migration_with_version(version, 'session_migration') : 'db/session_migration.rb'
         migration_template source, 'db/migrate/create_wechat_sessions.rb'
       end
