36768a9e4f8550813c1be55e9832c35e76f6601c
Joey Huang <4296411@qq.com>
2018-04-26 19:25:47 +0800

    1. add template message apis for miniprogram 2. add template message apis for wx

--- lib/wechat/api.rb
+++ lib/wechat/api.rb
@@ -12,5 +12,18 @@ module Wechat
     def template_message_send(message)
       post 'message/template/send', message.to_json, content_type: :json
     end
+
+    def list_message_template
+      get 'template/get_all_private_template'
+    end
+
+    def add_message_template(template_id_short)
+      post 'template/api_add_template', JSON.generate(template_id_short: template_id_short)
+    end
+
+    def del_message_template(template_id)
+      post 'template/del_private_template', JSON.generate(template_id: template_id)
+    end
+
   end
 end
--- lib/wechat/mp_api.rb
+++ lib/wechat/mp_api.rb
@@ -7,11 +7,31 @@ require 'wechat/concern/common'
 module Wechat
   class MpApi < ApiBase
     include Concern::Common
+    MPAPI_BASE = 'https://api.weixin.qq.com/cgi-bin/wxopen/'
 
     def template_message_send(message)
       post 'message/wxopen/template/send', message.to_json, content_type: :json
     end
 
+    def list_template_library(offset = 0, count = 20)
+      post 'template/library/list', JSON.generate(offset: offset, count: count)
+    end
+
+    def list_template_library_words(word_id)
+      post 'template/library/get', JSON.generate(id: word_id)
+    end
+
+    def add_message_template(template_id, keyword_id_list = [])
+      post 'template/add', JSON.generate(id: template_id, keyword_id_list: keyword_id_list)
+    end
+
+    def list_message_template(offset = 0, count = 20)
+      post 'template/list', JSON.generate(offset: offset, count: count)
+    end
+
+    def del_message_template(template_id)
+      post 'template/del', JSON.generate(template_id: template_id)
+    end
 
     def login(code)
       params = {
