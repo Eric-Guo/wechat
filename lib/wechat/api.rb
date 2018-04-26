a8cd4c5835112a5a50e4ee8af557d7a8f3df3278
Joey Huang <4296411@qq.com>
2018-04-26 18:58:50 +0800

    fix include error

--- lib/wechat/api.rb
+++ lib/wechat/api.rb
@@ -2,6 +2,7 @@ require 'wechat/api_base'
 require 'wechat/http_client'
 require 'wechat/token/public_access_token'
 require 'wechat/ticket/public_jsapi_ticket'
+require 'wechat/concern/common'
 
 module Wechat
   class Api < ApiBase
