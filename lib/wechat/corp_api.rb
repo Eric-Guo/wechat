c443d67c03ab723c8a20f112565327ce95e63f47
Eric-Guo <eric.guocz@gmail.com>
2020-01-07 17:52:36 +0800

    Add news_message_send similar to message_send to easy dev the qydev message sent.

--- lib/wechat/corp_api.rb
+++ lib/wechat/corp_api.rb
@@ -170,6 +170,15 @@ module Wechat
       post 'message/send', Message.to(userid).text(message).agent_id(agentid).to_json, content_type: :json
     end
 
+    def news_message_send(userid, title, description, link_url, pic_url)
+      post 'message/send', Message.to(userid).news([{
+        title: title,
+        description: description,
+        url: link_url,
+        pic_url: pic_url
+      }]).agent_id(agentid).to_json, content_type: :json
+    end
+
     def custom_message_send(message)
       post 'message/send', message.is_a?(Wechat::Message) ? message.agent_id(agentid).to_json : JSON.generate(message.merge(agent_id: agentid)), content_type: :json
     end
