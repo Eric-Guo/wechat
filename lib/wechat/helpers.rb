26484c68b94c5580412afda27de0186b449ff637
Eric-Guo <eric.guocz@gmail.com>
2020-10-02 11:19:39 +0800

    Wechat document say not including # part of the URL.
    https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html#62

--- lib/wechat/helpers.rb
+++ lib/wechat/helpers.rb
@@ -24,7 +24,7 @@ module Wechat
                  else
                    controller.request.original_url
                  end
-      page_url = page_url.split('#').first if ios?
+      page_url = page_url.split('#').first
       js_hash = api.jsapi_ticket.signature(page_url)
 
       config_js = <<~WECHAT_CONFIG_JS
@@ -38,12 +38,6 @@ module Wechat
         });
       WECHAT_CONFIG_JS
       javascript_tag config_js, type: 'application/javascript'
-    end
-
-    private
-
-    def ios?
-      controller.request.user_agent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
     end
   end
 end
