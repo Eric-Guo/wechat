59da5189e3a7e1f483ae4fd481247d64ae611dbb
Eric-Guo <eric.guocz@gmail.com>
2020-10-06 10:23:36 +0800

    qcloud function invokecloudfunction

--- lib/wechat/concern/qcloud.rb
+++ lib/wechat/concern/qcloud.rb
@@ -3,7 +3,8 @@
 module Wechat
   module Concern
     module Qcloud
-      def invokecloudfunction(name, post_body)
+      def invokecloudfunction(function_name, post_body)
+        post 'invokecloudfunction', post_body, params: { env: qcloud.qcloud_env, name: function_name }, base: Wechat::Api::TCB_BASE
       end
     end
   end
