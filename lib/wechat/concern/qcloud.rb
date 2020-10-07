3d5f337fa8b2fb12e76ba63590bb3dcb7101849e
Eric-Guo <eric.guocz@gmail.com>
2020-10-07 10:43:25 +0800

    CRUD of cloud DB and rename to more eye easier.

--- lib/wechat/concern/qcloud.rb
+++ lib/wechat/concern/qcloud.rb
@@ -3,11 +3,11 @@
 module Wechat
   module Concern
     module Qcloud
-      def invokecloudfunction(function_name, post_body_params)
+      def invoke_cloud_function(function_name, post_body_params)
         post 'invokecloudfunction', JSON.generate(post_body_params), params: { env: qcloud.qcloud_env, name: function_name }, base: Wechat::Api::TCB_BASE
       end
 
-      def databasemigrateimport(collection_name, file_path, file_type: Wechat::Qcloud::FILE_TYPE_JSON, stop_on_error: false, conflict_mode: Wechat::Qcloud::CONFLICT_MODE_UPSERT)
+      def qdb_migrate_import(collection_name, file_path, file_type: Wechat::Qcloud::FILE_TYPE_JSON, stop_on_error: false, conflict_mode: Wechat::Qcloud::CONFLICT_MODE_UPSERT)
         import_params_hash = { env: qcloud.qcloud_env,
                                collection_name: collection_name,
                                file_path: file_path,
@@ -17,7 +17,7 @@ module Wechat
         post 'databasemigrateimport', JSON.generate(import_params_hash), base: Wechat::Api::TCB_BASE
       end
 
-      def databasemigrateexport(query, file_path, file_type: Wechat::Qcloud::FILE_TYPE_JSON)
+      def qdb_migrate_export(query, file_path, file_type: Wechat::Qcloud::FILE_TYPE_JSON)
         export_params_hash = { env: qcloud.qcloud_env,
                                file_path: file_path,
                                file_type: file_type,
@@ -25,14 +25,14 @@ module Wechat
         post 'databasemigrateexport', JSON.generate(export_params_hash), base: Wechat::Api::TCB_BASE
       end
 
-      def databasemigratequeryinfo(job_id)
+      def qdb_migrate_query(job_id)
         query_info_hash = { env: qcloud.qcloud_env,
                             job_id: job_id }
 
         post 'databasemigratequeryinfo', JSON.generate(query_info_hash), base: Wechat::Api::TCB_BASE
       end
 
-      def updateindex(collection_name, create_indexes: [], drop_indexes: [])
+      def qdb_update_index(collection_name, create_indexes: [], drop_indexes: [])
         update_index_params_hash = { env: qcloud.qcloud_env,
                                      collection_name: collection_name,
                                      create_indexes: create_indexes,
@@ -40,16 +40,47 @@ module Wechat
         post 'updateindex', JSON.generate(update_index_params_hash), base: Wechat::Api::TCB_BASE
       end
 
-      def databasecollectionadd(collection_name)
+      def qdb_collection_add(collection_name)
         collection_add_params_hash = { env: qcloud.qcloud_env,
                                        collection_name: collection_name }
         post 'databasecollectionadd', JSON.generate(collection_add_params_hash), base: Wechat::Api::TCB_BASE
       end
 
-      def databasecollectiondelete(collection_name)
+      def qdb_collection_delete(collection_name)
         collection_delete_params_hash = { env: qcloud.qcloud_env,
                                           collection_name: collection_name }
         post 'databasecollectionadd', JSON.generate(collection_delete_params_hash), base: Wechat::Api::TCB_BASE
+      end
+
+      def qdb_collections(limit: 10, offset: 0)
+        get_collections_params_hash = { env: qcloud.qcloud_env,
+                                        limit: limit,
+                                        offset: offset }
+        post 'databasecollectionget', JSON.generate(get_collections_params_hash), base: Wechat::Api::TCB_BASE
+      end
+
+      def qdb_add(add_query)
+        post 'databaseadd', JSON.generate(env: qcloud.qcloud_env, query: add_query), base: Wechat::Api::TCB_BASE
+      end
+
+      def qdb_delete(delete_query)
+        post 'databasedelete', JSON.generate(env: qcloud.qcloud_env, query: delete_query), base: Wechat::Api::TCB_BASE
+      end
+
+      def qdb_update(update_query)
+        post 'databasedelete', JSON.generate(env: qcloud.qcloud_env, query: update_query), base: Wechat::Api::TCB_BASE
+      end
+
+      def qdb_query(query)
+        post 'databasequery', JSON.generate(env: qcloud.qcloud_env, query: query), base: Wechat::Api::TCB_BASE
+      end
+
+      def qdb_aggregate(aggregate_query)
+        post 'databaseaggregate', JSON.generate(env: qcloud.qcloud_env, query: aggregate_query), base: Wechat::Api::TCB_BASE
+      end
+
+      def qdb_count(count_query)
+        post 'databasecount', JSON.generate(env: qcloud.qcloud_env, query: count_query), base: Wechat::Api::TCB_BASE
       end
     end
   end
