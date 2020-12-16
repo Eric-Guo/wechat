803acd23c523308ad323b5564f9552869e0f1189
Big7lion <biglion77@outlook.com>
2020-12-16 10:24:15 +0800

    use base to set url

--- lib/wechat/mp_api.rb
+++ lib/wechat/mp_api.rb
@@ -12,7 +12,7 @@ module Wechat
   class MpApi < ApiBase
     def initialize(appid, secret, token_file, timeout, skip_verify_ssl, jsapi_ticket_file, qcloud_env, qcloud_token_file, qcloud_token_lifespan)
       super()
-      @client = HttpClient.new(Wechat::Api::WXA_API_BASE, timeout, skip_verify_ssl)
+      @client = HttpClient.new(Wechat::Api::API_BASE, timeout, skip_verify_ssl)
       @access_token = Token::PublicAccessToken.new(@client, appid, secret, token_file)
       @jsapi_ticket = Ticket::PublicJsapiTicket.new(@client, @access_token, jsapi_ticket_file)
       @qcloud = Qcloud::Token.new(@client, @access_token, qcloud_env, qcloud_token_file, qcloud_token_lifespan)
@@ -22,27 +22,33 @@ module Wechat
     include Concern::Qcloud
 
     def add_subscribe_message_template(template_id, keyword_id_list, scene_description = '')
-      post 'newtmpl/addtemplate', JSON.generate(tid: template_id, kidList: keyword_id_list, sceneDesc: scene_description)
+      post 'newtmpl/addtemplate',
+           JSON.generate(tid: template_id, kidList: keyword_id_list, sceneDesc: scene_description),
+           base: Wechat::Api::WXA_API_BASE
     end
 
     def delete_subscribe_message_template(template_id)
-      post 'newtmpl/deltemplate', JSON.generate(priTmplId: template_id)
+      post 'newtmpl/deltemplate', JSON.generate(priTmplId: template_id), base: Wechat::Api::WXA_API_BASE
     end
 
     def get_subscribe_message_category
-      get 'newtmpl/getcategory'
+      get 'newtmpl/getcategory', base: Wechat::Api::WXA_API_BASE
     end
 
     def get_public_template_keywords(template_id)
-      get 'newtmpl/getpubtemplatekeywords', params: { tid: template_id }
+      get 'newtmpl/getpubtemplatekeywords',
+          params: { tid: template_id },
+          base: Wechat::Api::WXA_API_BASE
     end
 
     def get_public_template_title_list(ids, offset: 0, count: 20)
-      get 'newtmpl/getpubtemplatetitles', params: { ids: ids, start: offset, limit: count }
+      get 'newtmpl/getpubtemplatetitles',
+          params: { ids: ids, start: offset, limit: count },
+          base: Wechat::Api::WXA_API_BASE
     end
 
     def get_template_list
-      get 'newtmpl/gettemplate'
+      get 'newtmpl/gettemplate', base: Wechat::Api::WXA_API_BASE
     end
 
     def send_subscribe_message(message)
