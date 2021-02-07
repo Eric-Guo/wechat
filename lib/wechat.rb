42076cb35645e9e754c3f9f66c655dae3268be7f
Eric-Guo <eric.guocz@gmail.com>
2021-02-07 17:44:08 +0800

    Another rubocop -A fix.

--- lib/wechat.rb
+++ lib/wechat.rb
@@ -50,7 +50,7 @@ module Wechat
     decrypted_data = Base64.decode64(encrypted_data)
     JSON.parse(cipher.update(decrypted_data) + cipher.final)
   rescue StandardError => e
-    { 'errcode': 41003, 'errmsg': e.message }
+    { errcode: 41003, errmsg: e.message }
   end
 end
 
