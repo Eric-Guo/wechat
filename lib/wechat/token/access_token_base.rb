49cadc3469c37e2ac550a2868ccc12b182cb3ea5
DrinE Liu <drine.liu@external.tablecheck.com>
2023-01-11 13:12:31 +0800

    Record based token

--- lib/wechat/token/access_token_base.rb
+++ lib/wechat/token/access_token_base.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
-
 module Wechat
   module Token
     class AccessTokenBase
-      attr_reader :client, :appid, :secret, :token_file, :access_token, :token_life_in_seconds, :got_token_at
+      attr_reader :client, :appid, :secret, :token_file, :access_token, :token_life_in_seconds, :got_token_at, :record
 
-      def initialize(client, appid, secret, token_file)
+      def initialize(client, appid, secret, token_file, record = nil)
         @appid = appid
         @secret = secret
         @client = client
         @token_file = token_file
+        @record = record
         @random_generator = Random.new
       end
 
@@ -40,15 +40,56 @@ module Wechat
       end
 
       def read_token
-        JSON.parse(File.read(token_file))
+        if record_based_token?
+          throw_error_if_missing_attributes!
+
+          {
+            'access_token' => record.access_token,
+            'got_token_at' => record.got_token_at,
+            'token_expires_in' => record.token_expires_in
+          }
+        else
+          JSON.parse(File.read(token_file))
+        end
       end
 
       def write_token(token_hash)
-        File.write(token_file, token_hash.to_json)
+        if record_based_token?
+          write_token_to_record(token_hash)
+        else
+          File.write(token_file, token_hash.to_json)
+        end
       end
 
       def remain_life_seconds
         token_life_in_seconds - (Time.now.to_i - got_token_at)
+      end
+
+      private
+
+      def record_based_token?
+        record.present?
+      end
+
+      def write_token_to_record(token_hash)
+        throw_error_if_missing_attributes!
+
+        record.access_token = token_hash['access_token']
+        record.token_expires_in = token_hash['token_expires_in']
+        record.got_token_at = Time.now
+        record.save || record.save(validate: false)
+      end
+
+      def missing_necessary_attributes?
+        return true unless record.respond_to?(:access_token)
+        return true unless record.respond_to?(:token_expires_in)
+        return true unless record.respond_to?(:got_token_at)
+
+        false
+      end
+
+      def throw_error_if_missing_attributes!
+        raise "Missing attributes #access_token or #token_expires_in or #got_token_at in #{record.class.name}" if missing_necessary_attributes?
       end
     end
   end
--- spec/lib/wechat/public_access_token_spec.rb
+++ spec/lib/wechat/public_access_token_spec.rb
@@ -4,9 +4,14 @@ RSpec.describe Wechat::Token::PublicAccessToken do
   let(:token_file) { Rails.root.join('access_token') }
   let(:token) { '12345' }
   let(:client) { double(:client) }
+  let(:record) { OpenStruct.new(access_token: '123', token_expires_in: '7200', got_token_at: '2022-12-19 17:34:11 +0900') }
+  let(:time) { Time.zone.parse('2022-12-19T15:00:00+0800') }
 
-  subject do
+  subject :file_based_subject do
     Wechat::Token::PublicAccessToken.new(client, 'appid', 'secret', token_file)
+  end
+  subject :record_based_subject do
+    Wechat::Token::PublicAccessToken.new(client, 'appid', 'secret', token_file, record)
   end
 
   before :each do
@@ -14,6 +19,7 @@ RSpec.describe Wechat::Token::PublicAccessToken do
       .with('token', params: { grant_type: 'client_credential',
                                appid: 'appid',
                                secret: 'secret' }).and_return('access_token' => '12345', 'expires_in' => 7200)
+    allow(record).to receive(:save).and_return(true)
   end
 
   after :each do
@@ -23,52 +29,76 @@ RSpec.describe Wechat::Token::PublicAccessToken do
   describe '#token' do
     specify 'read from file if access_token is not initialized' do
       File.open(token_file, 'w') { |f| f.write({ 'access_token' => '12345', 'expires_in' => 7200 }.to_json) }
-      expect(subject.token).to eq('12345')
+      expect(file_based_subject.token).to eq('12345')
+    end
+
+    specify 'read from attributes if access_token is not initialized' do
+      File.open(token_file, 'w') { |f| f.write({ 'access_token' => '12345', 'expires_in' => 7200 }.to_json) }
+      expect(record_based_subject.token).to eq(token)
     end
 
     specify "refresh access_token if token file didn't exist" do
       expect(File.exist? token_file).to be false
-      expect(subject.token).to eq('12345')
+      expect(file_based_subject.token).to eq('12345')
       expect(File.exist? token_file).to be true
     end
 
     specify 'refresh access_token if token file is invalid' do
       File.open(token_file, 'w') { |f| f.write('rubbish') }
-      expect(subject.token).to eq('12345')
+      expect(file_based_subject.token).to eq('12345')
     end
 
     specify 'raise exception if refresh failed' do
       allow(client).to receive(:get).and_raise('error')
-      expect { subject.token }.to raise_error('error')
+      expect { file_based_subject.token }.to raise_error('error')
+      expect { record_based_subject.token }.to raise_error('error')
     end
   end
 
   describe '#refresh' do
     specify 'will set access_token' do
-      expect(subject.refresh).to eq(token)
-      expect(subject.access_token).to eq('12345')
+      expect(file_based_subject.refresh).to eq(token)
+      expect(file_based_subject.access_token).to eq('12345')
+    end
+
+    specify 'will set access_token, token_life_in_seconds, got_token_at' do
+      allow(Time).to receive(:now).and_return(time)
+      expect(record_based_subject.refresh).to eq(token)
+      expect(record_based_subject.token).to eq('12345')
+      expect(record_based_subject.access_token).to eq('12345')
+      expect(record_based_subject.token_life_in_seconds).to eq(7200)
+      expect(record_based_subject.got_token_at).to eq(time.to_i)
     end
 
     specify "won't set access_token if request failed" do
       allow(client).to receive(:get).and_raise('error')
 
-      expect { subject.refresh }.to raise_error('error')
-      expect(subject.access_token).to be_nil
+      expect { file_based_subject.refresh }.to raise_error('error')
+      expect(file_based_subject.access_token).to be_nil
+
+      expect { record_based_subject.refresh }.to raise_error('error')
+      expect(record_based_subject.access_token).to be_nil
     end
 
     specify "won't set access_token if response value invalid" do
       allow(client).to receive(:get).and_return('rubbish')
 
-      expect { subject.refresh }.to raise_error(Wechat::InvalidCredentialError)
-      expect(subject.access_token).to be_nil
+      expect { file_based_subject.refresh }.to raise_error(Wechat::InvalidCredentialError)
+      expect(file_based_subject.access_token).to be_nil
+
+      expect { record_based_subject.refresh }.to raise_error(Wechat::InvalidCredentialError)
+      expect(record_based_subject.access_token).to be_nil
     end
 
     # it will be nil(content_length 0) if appid and secret is invalid
     specify "won't set access_token if response value is nil" do
       allow(client).to receive(:get).and_return(nil)
 
-      expect { subject.refresh }.to raise_error(Wechat::InvalidCredentialError)
-      expect(subject.access_token).to be_nil
+      expect { file_based_subject.refresh }.to raise_error(Wechat::InvalidCredentialError)
+      expect(file_based_subject.access_token).to be_nil
+
+      expect { record_based_subject.refresh }.to raise_error(Wechat::InvalidCredentialError)
+      expect(record_based_subject.access_token).to be_nil
     end
   end
 end
